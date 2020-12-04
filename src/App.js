2961c27533385dd5847b8531247ebabdd8b9ae8e
zabik <marcin.zabicki@polskieradio.pl>
2020-12-04 15:22:41 +0100

    hidden menu, routing with params

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Main';
 import Dashboards from './components/Dashboards/Main';
-import Inbox from './components/Inbox/Main';
-import Products from './components/Products/Main';
-import Invoices from './components/Invoices/Main';
+// import Inbox from './components/Inbox/Main';
+// import Products from './components/Products/Main';
+// import Invoices from './components/Invoices/Main';
 import Header from './components/Header';
-import Nav from './components/Nav';
+// import Nav from './components/Nav';
 const App = () => {
   return (
     <>
       <Header />
       <BrowserRouter>
         <div className="content-wrapper">
-          <Nav />
+          {/* <Nav /> */}
           <section className="content">
-            <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/:machineName">
+              <Dashboards />
+            </Route>
+            {/* <Switch>
               <Route exact path="/" component={Home} />
               <Route path="/dashboards" component={Dashboards} />
               <Route exact path="/inbox" component={Inbox} />
               <Route path="/products" component={Products} />
               <Route exact path="/invoices" component={Invoices} />
-            </Switch>
+            </Switch> */}
           </section>
         </div>
       </BrowserRouter>

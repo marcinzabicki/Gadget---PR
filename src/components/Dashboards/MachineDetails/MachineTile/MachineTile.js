4e97526937f9d86b19205942ed495e5b84c148fb
Marcin Mojski <marcin_mojski@interia.pl>
2020-12-08 15:00:58 +0100

    hook sitch metric

--- src/components/Dashboards/MachineDetails/MachineTile/MachineTile.js
+++ src/components/Dashboards/MachineDetails/MachineTile/MachineTile.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Chart from '../Chart'
 import MetricTile from '../MetricTile'
 import Label from '../Label'
 import '../MachineDetails.css'
 import { Link } from 'react-router-dom'
 
-const changeMetricData = (event) => {
-    console.log(event.target.value);
-}
+
+
 
 const MachineTile = (props) => {
+
+    const [machineTileState, setMachineTileState] = useState({
+        metrics: {"RAM":props.ram, "CPU": props.cpu},
+        show:"CPU"
+    })
+    
+    const changeMetricData = (event) => {
+        setMachineTileState({
+        metrics: {"RAM":props.ram, "CPU": props.cpu},
+        show: event.target.value
+        })
+    }
     return (
         <div className="machine-tile">
             <Link to={`/${props.machine}`}>
@@ -19,7 +30,11 @@ const MachineTile = (props) => {
                 <MetricTile values={props.services} type="service">Services</MetricTile>
                 <MetricTile values={props.disc} type="disc">Disc space</MetricTile>
             </div>
-            <Chart changed={(event) => changeMetricData(event)} selectable={true}>{props.cpu}</Chart>
+            <Chart 
+                changed={(event) => changeMetricData(event)} 
+                selectable={true}>
+                {parseInt(machineTileState.metrics[machineTileState.show])}
+            </Chart>
         </div>
 
     )

cf66d8661cf8b6b0a7e1b44744edd952b2c28055
zabik <marcin.zabicki@polskieradio.pl>
2020-12-10 15:13:39 +0100

    fetch services from API

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { MACHINE_LIST_URL } from "../config";
+import { SERVICES_LIST_URL } from "../config";
 
 export class API {
   static async fetchMachineList() {
@@ -12,11 +13,11 @@ export class API {
       console.log(e);
     }
   }
-  static async fetchServicesList({ machineId = "0" } = {}) {
+  static async fetchServicesList({ machineName } = {}) {
     try {
       return await axios({
         method: "GET",
-        url: `${MACHINE_LIST_URL}/${machineId}`,
+        url: `${SERVICES_LIST_URL}/${machineName}`,
       });
     } catch (e) {
       console.log(e);

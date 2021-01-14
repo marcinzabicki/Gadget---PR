e654e7b0a44acad33d6abf1658eb41521c8508b5
Marcin Mojski <marcin_mojski@interia.pl>
2021-01-14 20:52:47 +0100

    home mobile not yet

--- src/components/Home/Main.js
+++ src/components/Home/Main.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect, useContext } from 'react'
 import MachineTile from '../Dashboards/MachineDetails/MachineTile/MachineTile'
+import MachineTileMobile from '../Dashboards/MachineDetails/MachineTile/MachineTileMobile'
 import '../Dashboards/MachineDetails/MachineDetails.css';
 //import Logs from '../Dashboards/Logs/Logs'
 import { API } from '../../utils/API'
 import { SignalRContext } from '../../utils/signalr-context';
+import { useWindowSize } from "../../Hooks";
+
+
+
 const Home = () => {
-
     const [machineListState, setMachineListState] = useState([])
-
     const connection = useContext(SignalRContext);
+    const windowSize = useWindowSize();
 
 
     useEffect(() => {
@@ -17,6 +21,13 @@ const Home = () => {
             setMachineListState(response.data);
         });
     }, []);
+
+    function calculateMachineStatus(machine){
+        if(machine.cpu>0.7 || machine.ram>0.75 || machine.discPercentage>0.95) return "#E13849";
+        if(machine.cpu>0.6 || machine.ram>0.6 || machine.discPercentage>0.7) return "#F0AD4E";
+        if(machine.cpu>0 || machine.ram>0 || machine.discPercentage>0) return "#38E18D";
+        return "#4B4E75"
+    }
 
 
     useEffect(() => {
@@ -29,25 +40,44 @@ const Home = () => {
                 updated[index].cpu = response.cpuPercentUsage;
                 updated[index].ram = 100 * (1 - (response.memoryFree / response.memoryTotal));
                 updated[index].disc = `${Math.floor(response.discOccupied)}/${Math.floor(response.discTotal)}`;
+                updated[index].discPercentage = Math.floor(response.discOccupied/response.discTotal)
                 updated[index].services = `${response.servicesRunning}/${response.servicesCount}`
                 setMachineListState(updated)
             });
         }
     }, [connection]);
 
-    const machines = machineListState.map((m, i) => {
-        return (
-            <MachineTile
-                machine={m["name"]}
-                cpu={m["cpu"]}
-                ram={m["ram"]}
-                disc={m["disc"]}
-                services={m["services"]}
-                key={i}
-                machineAddress={m["address"]}>
-            </MachineTile>
-        )
-    })
+let machines = {};
+    windowSize<=768?(
+         machines = machineListState.map((m, i) => {
+            return (
+                <MachineTileMobile
+                    machine={m["name"]}
+                    key={i}
+                    order={i +1}
+                    color={calculateMachineStatus(m)}
+                    machineAddress={m["address"]}>
+                </MachineTileMobile>
+            )
+        })
+    ):
+    (
+         machines = machineListState.map((m, i) => {
+            return (
+                <MachineTile
+                    machine={m["name"]}
+                    cpu={m["cpu"]}
+                    ram={m["ram"]}
+                    disc={m["disc"]}
+                    services={m["services"]}
+                    key={i}
+                    machineAddress={m["address"]}>
+                </MachineTile>
+            )
+        })
+    );
+
+    
 
     // const tmpLogs = [
     //     {time:"09:14:33", status:"[Information]", message:"Lorem ipsum dsadasdsa", service:"nmvsasdasd"},

2cbab5f50b56d168a3124aa74b6b09495d3eb67d
Marcin Mojski <marcin_mojski@interia.pl>
2021-03-03 11:49:36 +0100

    modal in mobile start, stop and restart

--- src/components/Dashboards/ServiceMobile.js
+++ src/components/Dashboards/ServiceMobile.js
@@ -2,22 +2,39 @@ import { API } from '../../utils/API'
 import React, { useState } from 'react';
 import Modal from 'react-modal';
 import ApprovalModal from '../Common/ApprovalModal'
+import { Link } from "react-router-dom";
 
 const ServiceMobile = ({ agent, service, index }) => {
-    const [showModal, setShowModal] = useState(false);
+    const [showRestartModal, setShowRestartModal] = useState(false);
+    const [showStartModal, setShowStartModal] = useState(false);
+    const [showStopModal, setShowStopModal] = useState(false);
     Modal.defaultStyles.overlay.backgroundColor = 'transparent';
 
-    const showModalHandler = () => {
-        let isShowing = showModal;
-        setShowModal(!isShowing);
-    };
-
+    const hideModal = (name)=>{
+        switch(name) {
+           case 'restart' : setShowRestartModal(false);
+           break;
+           case 'start': setShowStartModal(false);
+           break;
+           case'stop':setShowStopModal(false);
+           break;
+        }
+    }
 
     const restartServiceHandler = ()=>{
-        console.log("ffdsgfgdfdfgfgfgfgfgfgfgfgfgfgf");
-          API.restartService(agent,service.name);
-          setShowModal(false);
-      }
+        API.restartService(agent,service.name);
+        setShowRestartModal(false);
+    }
+
+    const startServiceHandler = ()=>{
+        API.startService(agent,service.name);
+        setShowStartModal(false);
+    }
+
+    const stopServiceHandler = ()=>{
+        API.stopService(agent,service.name);
+        setShowStopModal(false);
+    }
 
     return (
         <>
@@ -28,27 +45,55 @@ const ServiceMobile = ({ agent, service, index }) => {
                     <p className="service-name-mobile">{service.name}</p>
                     <div>
                         {service.status.toLowerCase() === "running" ? (
-                            <button className="button" onClick={() => API.stopService(agent, service.name)} >Stop</button>
+                            <button className="button" onClick={()=>setShowStopModal(true)} >Stop</button>
                         ) : (
-                                <button className="button" onClick={() => API.startService(agent, service.name)} >Start</button>
+                                <button className="button" onClick={()=>setShowStartModal(true)} >Start</button>
                             )}
 
-                        <button className="button" onClick={showModalHandler}>Restart</button>
-                        <button className="button special">Show logs</button>
+                        <button className="button" onClick={()=>setShowRestartModal(true)}>Restart</button>
+                        <Link to={`/${agent}/${service.name}`}>
+                            <button className="button special">More</button>
+                        </Link>
                     </div>
                 </div>
             </div>
             <Modal
-                isOpen={showModal}
+                isOpen={showRestartModal}
                 ariaHideApp={false}
                 className="agent-modal"
             >
                 <ApprovalModal 
                 service={service.name} 
-                decline={showModalHandler}
+                decline={()=>{hideModal('restart')}}
                 message="Are you sure you want to restart service?"
                 entity={service.name}
                 action={restartServiceHandler}>
+                </ApprovalModal>
+            </Modal>
+            <Modal
+                isOpen={showStartModal}
+                ariaHideApp={false}
+                className="agent-modal"
+            >
+                <ApprovalModal 
+                service={service.name} 
+                decline={()=>{hideModal('start')}}
+                message="Are you sure you want to start service?"
+                entity={service.name}
+                action={startServiceHandler}>
+                </ApprovalModal>
+            </Modal>
+            <Modal
+                isOpen={showStopModal}
+                ariaHideApp={false}
+                className="agent-modal"
+            >
+                <ApprovalModal 
+                service={service.name} 
+                decline={()=>{hideModal('stop')}}
+                message="Are you sure you want to stop service?"
+                entity={service.name}
+                action={stopServiceHandler}>
                 </ApprovalModal>
             </Modal>
         </>
